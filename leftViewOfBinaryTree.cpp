fb5fb414b0d1df8cc4d34575cde9ff3710f99a5a
vector <37574448+tangent108@users.noreply.github.com>
2020-01-31 18:19:37 +0530

    Add files via upload

---
+++ leftViewOfBinaryTree.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<queue>
+#define null 0
+
+using namespace std;
+
+class Node{
+    public:
+    Node *left;
+    int data;
+    Node *right;
+    Node(){}
+    Node(int data){
+        this->data = data;
+        left=right=null;
+    }
+};
+
+void leftViewOfBinaryTree(Node *root){
+    if(root==null){
+        return ;
+    }
+    else{
+        queue<Node *> q;
+        q.push(root);
+        while(!q.empty()){
+
+            int queueSize = q.size();
+            int count = queueSize;
+            while(count > 0){
+                Node *node = q.front();
+                q.pop();
+                if(count==queueSize){
+                    cout << node->data<<" ";
+                }
+
+                if(node->left!=null){
+                    q.push(node->left);
+                }
+                if(node->right!=null){
+                    q.push(node->right);
+
+                }
+                count--;
+            }
+
+        }
+    }
+}
+
+int main(){
+    Node * root = new Node(1);
+     root->left = new Node(2);
+     root->right = new Node(3);
+     root->left->left = new Node(4);
+     root->left->right= new Node(5);
+     root->right->left= new Node(6);
+     root->right->left->left= new Node(10);
+     root->right->right= new Node(7);
+     root->right->right->left = new Node(8);
+     root->right->right->right = new Node(9);
+
+     cout <<"\nThe left view of tree \n";
+     leftViewOfBinaryTree(root);
+}
