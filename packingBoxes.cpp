f7ae9d0da9001b73406545b801b2a24d5f8acc8d
vector <37574448+tangent108@users.noreply.github.com>
2020-02-29 16:37:38 +0530

    Create packingBoxes.cpp

---
+++ packingBoxes.cpp
@@ -0,0 +1,61 @@
+/*
+
+Given an array of integers, pack consecutive elements into sublists.
+
+For example, given the list [4, 4, 1, 6, 6, 6, 1, 1, 1, 1], return [[4, 4], [1], [6, 6, 6], [1, 1, 1, 1]].
+
+Note: If there's only one occurrence in the list it should still be in its own sublist.
+
+Example 1
+Input
+
+nums = [4, 4, 1, 6, 6, 6, 1, 1, 1, 1]
+Output
+
+[[4, 4],
+[1],
+[6, 6, 6],
+[1, 1, 1, 1]]
+Example 2
+Input
+
+nums = [5, 5, 5, 5, 5, 5, 5, 5]
+Output
+
+[[5, 5, 5, 5, 5, 5, 5, 5]]
+Example 3
+Input
+
+nums = [1]
+Output
+
+[[1]]
+
+*/
+
+vector<vector<int>> solve(vector<int>& nums) {
+    // Write your code here
+    
+    vector<vector<int>> vv;
+    for(int i=0;i<nums.size();i++){
+        vector<int> v;
+        if(nums[i]==nums[i+1]){
+            int ele = nums[i];
+            v.push_back(ele);
+            while(nums[i+1]==ele){
+                v.push_back(nums[i+1]);
+                i++;
+                
+            }
+            vv.push_back(v);
+        }
+        else{
+            vector<int> v1;
+            v1.push_back(nums[i]);
+            vv.push_back(v1);
+        }
+       
+    }
+    return vv;
+    
+};
